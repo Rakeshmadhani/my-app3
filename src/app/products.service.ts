b481034be7550f14b7e77c9ee0e409b2e1cb66ef
unknown <rockyrakesh4297@gmail.com>
2023-07-25 12:28:45 +0530

    custom data type products are implemented

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Product } from './product';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class ProductsService {
 
   constructor(private httpclient:HttpClient) { }
 
-  getProducts():Observable<any>{
-    return this.httpclient.get('/assets/product.json');
+  getProducts():Observable<Product[]>{
+    return this.httpclient.get<Product[]>('/assets/product.json');
   }
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ProductsService } from '../products.service';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-products',
@@ -8,11 +9,11 @@ import { ProductsService } from '../products.service';
 })
 export class ProductsComponent {
 
-  public products: any = [];
+  public products: Product[]= [];
   
   constructor(private productsService:ProductsService){
     this.productsService.getProducts().subscribe(
-      (data:any)=>{
+      (data:Product[])=>{
         this.products = data;
       },
       (err:any)=>{
